2fe5668a9134c5be1cccf28da22ac3a4a95165bf
rim <devrealticon.gmail.com>
2019-02-27 11:38:02 +0900

    주석수정(안봐도 됨)

--- app/src/main/java/kr/co/teada/ex88firebasechatting/G.java
+++ app/src/main/java/kr/co/teada/ex88firebasechatting/G.java
@@ -3,5 +3,5 @@ package kr.co.teada.ex88firebasechatting;
 public class G {
 
     public static  String nickName;
-    public static  String profileUrl; //다운로드 경로
+    public static  String profileUrl; //프로필 사진 : 다운로드 경로
 }
